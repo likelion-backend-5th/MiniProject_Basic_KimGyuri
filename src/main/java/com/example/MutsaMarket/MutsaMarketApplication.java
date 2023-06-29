c1f7a589b5916c49a4bf35a646720787e6d4615f
kkyuwoo <ggya5@naver.com>
2023-06-29 15:29:39 +0900

    First Commit

---
+++ src/main/java/com/example/MutsaMarket/MutsaMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.MutsaMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutsaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutsaMarketApplication.class, args);
+	}
+
+}
