0b68c55414007ba77dfbcd67df37062cf8b9a22f
kkyuwoo <ggya5@naver.com>
2023-07-03 16:10:43 +0900

    refactor: 물품 정보 등록 시 사용자 입력에 대한 유효성 검증 메시지 내용 변경

--- src/main/java/com/example/MutsaMarket/dto/SalesItemDto.java
+++ src/main/java/com/example/MutsaMarket/dto/SalesItemDto.java
@@ -6,19 +6,19 @@ import lombok.Data;
 
 @Data
 public class SalesItemDto {
-    @NotBlank(message = "제목을 확인해주세요.")
+    @NotBlank(message = "제목을 입력해주세요.")
     private String title;
 
-    @NotBlank(message = "물품 설명을 확인해주세요.")
+    @NotBlank(message = "물품 설명을 입력해주세요.")
     private String description;
 
-    @NotNull(message = "가격을 확인해주세요.")
+    @NotNull(message = "가격을 입력해주세요.")
     private Long minPriceWanted;
 
-    @NotBlank(message = "작성자를 확인해주세요.")
+    @NotBlank(message = "작성자를 입력해주세요.")
     private String writer;
 
-    @NotBlank(message = "비밀번호를 확인해주세요.")
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 
     public static SalesItemDto fromEntity(SalesItemEntity entity) {
