a4cb307f34d4aa59883544644f24d19f1d7a2556
kkyuwoo <ggya5@naver.com>
2023-07-04 18:52:34 +0900

    refactor: import 정리

--- src/main/java/com/example/MutsaMarket/repository/CommentRepository.java
+++ src/main/java/com/example/MutsaMarket/repository/CommentRepository.java
@@ -3,7 +3,5 @@ package com.example.MutsaMarket.repository;
 import com.example.MutsaMarket.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
 }
